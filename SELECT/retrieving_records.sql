a1bbd078d391773fb17f347c8dc8fa0aafc20965
ankitforcodes <ankitforcodes@gmail.com>
2023-06-03 17:55:09 +0530

    Update retrieving_records.sql

--- SELECT/retrieving_records.sql
+++ SELECT/retrieving_records.sql
@@ -28,3 +28,18 @@ select sal as salary, comm as commission
 select concat('Name of employee with ID', id,' is ', ename) as msg
    from emp
    where deptno=10;
+
+
+
+-- CASE
+-- You want to perform IF-ELSE operations on values in your SELECT statement. For
+-- example, you would like to produce a result set such that if an employee is paid
+-- $2,000 or less, a message of “UNDERPAID” is returned; if an employee is paid $4,000
+-- or more, a message of “OVERPAID” is returned; and if they make somewhere in
+-- between, then “OK” is returned
+select ename,sal,
+     case when sal <= 2000 then 'UNDERPAID'
+          when sal >= 4000 then 'OVERPAID'
+          else 'OK'
+     end as status
+from emp
